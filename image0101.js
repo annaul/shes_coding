a29cd1b3677ccf3f90dd015aa73194653e70ce29
Anna Ulanova <anetteua0@gmail.com>
2017-06-08 13:46:00 -0700

    changed zeroArray to two dimentional

--- image0101.js
+++ image0101.js
@@ -15,7 +15,7 @@ function iterate(arr) {
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr.length; j++) {
       if (arr[i][j] === 0) {
-        zeroArr.push(i, j);
+        zeroArr.push([i, j]);
       }
     }
   }
@@ -24,14 +24,14 @@ function iterate(arr) {
 console.log('zeroArr: ', iterate(img));
 
 function findWidth(arr) {
-  var width = zeroArr[zeroArr.length-1] - zeroArr[1] + 1;
+  var width = zeroArr[zeroArr.length-1][1] - zeroArr[0][1] + 1;
   return width;
 }
 
 console.log('width: ', findWidth(zeroArr));
 
 function findHight(arr) {
-  var height = zeroArr[zeroArr.length-2] - zeroArr[0] + 1;
+  var height = zeroArr[zeroArr.length-1][0] - zeroArr[0][0] + 1;
   return height;
 }
 console.log('height: ', findHight(zeroArr));
